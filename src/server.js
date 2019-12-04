d2b4a6351095b6e1246588d67f75a92780726d10
Máximo Lanza <54601369+maximolanza@users.noreply.github.com>
2019-12-04 08:07:44 -0300

    Configurando server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
+/*const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(process.env.PORT || 8080);
+
+  app.use(express.static(__dirname+'/dist/webcv'));
+  app.get('/',function(req,res){
+      res.sendFile(path.join(__dirname+'/dist/webcv/index.html'));
+  });
+  
+
+  app.use('/*',function(req, res) {
+    res.sendfile(__dirname + '../../dist/index.html');
+});
+*/
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/navidadhb'));
+app.use(express.static(__dirname + '../..//dist/navidadhb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/navidadhb/index.html'));
+res.sendFile(path.join(__dirname+'../..//dist/navidadhb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
