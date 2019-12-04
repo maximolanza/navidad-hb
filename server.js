acccd0c0b4cc7f3f3a820a8254f0e0b7d58f7409
Máximo Lanza <54601369+maximolanza@users.noreply.github.com>
2019-12-04 08:21:50 -0300

    Configurando server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -14,21 +14,22 @@ app.listen(process.env.PORT || 8080);
   app.use('/*',function(req, res) {
     res.sendfile(__dirname + '../../dist/index.html');
 });
-*/
 
+/*
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '../..//dist/navidadhb'));
+app.use(express.static(__dirname + '../../dist/navidadhb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'../..//dist/navidadhb/index.html'));
+res.sendFile(path.join(__dirname+'../../dist/navidadhb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
+*/
